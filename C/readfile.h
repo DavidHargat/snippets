5afa338fa1278396b4e03ac893e52d4ef6804ff7
david hargat <davidhargat@yahoo.com>
2016-05-31 23:40:33 -0400

    readfile cleanup

--- C/readfile.h
+++ C/readfile.h
@@ -1,9 +1,7 @@
-#ifndef READFILE_H
-#define READFILE_H
-
 #include <stdlib.h>
 #include <stdio.h>
 
+// beware, error checking is ommitted for clarity
 char *readfile(char *filename){
 	FILE *f;
 	char *buf;
@@ -13,15 +11,18 @@ char *readfile(char *filename){
 		return NULL;
 	
 	fseek(f, 0L, SEEK_END);	
+	
 	size = ftell(f);
+	
 	fseek(f, 0L, SEEK_SET);	
+	
 	buf = malloc(size+1);
+	
 	fread(buf, 1, size, f);
+	
 	buf[size] = 0;
 
 	fclose(f);
 
 	return buf;
 }
-
-#endif
